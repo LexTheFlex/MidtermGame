373487a41170d0de78587c48f5eaaa8e8bca9d38
Alex Romoff <air244@nyu.edu>
2015-07-30 04:48:37 -0400

    Initial commit

---
+++ Assets/scripts/Destroy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject human;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag=="player")
+			Destroy(human.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
---
+++ Assets/scripts/playerControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+
+	public float moveSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position += new Vector3 (0, 0, 1) * moveSpeed *Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.position -= new Vector3 (0, 0, 1) * moveSpeed *Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position += new Vector3 (1, 0, 0) * moveSpeed *Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position -= new Vector3 (1, 0, 0) * moveSpeed *Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.Space)) {
+			transform.position += new Vector3 (0, 0, 3) * moveSpeed * Time.deltaTime;
+		} 
+
+		//Rigidbody rbody = GetComponent<Rigidbody> ();
+
+
+	
+	}
+}
